654d5f34cdf9b0fc2a327edf3ee14c40a1cd80b2
lucasdichiara21 <116927266+lucasdichiara21@users.noreply.github.com>
2024-06-13 20:21:22 -0300

    Update HashCodeSha1.java
    ACTUALIZACIÓN HASH DE SHA-1 a SHA-256

--- src/main/java/Giftify/Giftify/Controllers/HashCodeSha1.java
+++ src/main/java/Giftify/Giftify/Controllers/HashCodeSha1.java
@@ -4,9 +4,9 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class HashCodeSha1 {
-    public static String SHA1 (String pass){
+    public static String SHA256 (String pass){
         try {
-        MessageDigest sha1=MessageDigest.getInstance("SHA-1");
+        MessageDigest sha256=MessageDigest.getInstance("SHA-256");
         byte [] MessageBytes = pass.getBytes();
         byte [] HashBytes = sha1.digest(MessageBytes);
         
@@ -15,8 +15,8 @@ public class HashCodeSha1 {
         for (byte b : HashBytes){
             sb.append(String.format("%02x",b));       
         }
-        String sha1Hash=sb.toString();
-        return sha1Hash;
+        String sha256Hash=sb.toString();
+        return sha256Hash;
         }catch (NoSuchAlgorithmException e) {
             throw new RuntimeException (e);
         }
