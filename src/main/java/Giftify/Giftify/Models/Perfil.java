a7a06540e854575970a388721a8dec2e9f5a0885
Cristian Ezequiel Ortiz <cristianortiz367@gmail.com>
2024-06-20 22:31:17 -0300

    Correccion de pagina de estilos en index

--- src/main/java/Giftify/Giftify/Models/Perfil.java
+++ src/main/java/Giftify/Giftify/Models/Perfil.java
@@ -5,7 +5,11 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import java.time.LocalDate;
+import lombok.Getter;
+import lombok.Setter;
 
+@Setter
+@Getter
 @Entity
 public class Perfil {
 
@@ -16,47 +20,6 @@ public class Perfil {
     private String apellido;
     private LocalDate fechaNacimiento;
     private int usuarioId;
-
-    // Getters and setters
-    public int getId() {
-        return idPerfil;
-    }
-
-    public void setId(int id) {
-        idPerfil= id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getApellido() {
-        return apellido;
-    }
-
-    public void setApellido(String apellido) {
-        this.apellido = apellido;
-    }
-
-    public LocalDate getFechaNacimiento() {
-        return fechaNacimiento;
-    }
-
-    public void setFechaNacimiento(LocalDate fechaNacimiento) {
-        this.fechaNacimiento = fechaNacimiento;
-    }
-
-    public int getUsuarioId() {
-        return usuarioId;
-    }
-
-    public void setUsuarioId(int usuarioId) {
-        this.usuarioId = usuarioId;
-    }
 
     public Perfil() {}
 
